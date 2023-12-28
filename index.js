18840aa11702e4651a6e39f083f336174746411b
grazulisedgars <grazulisedgars@gmail.com>
2023-12-28 15:04:08 +0200

    Adds prompt part for Manager and calls it, questions come up in node

--- index.js
+++ index.js
@@ -13,7 +13,50 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const teamMembers = [];
 
+// Prompt questions for Manager
+function promptManager() {
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the team manager's name?",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the team manager's id?",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the team manager's email?",
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is the team manager's office number?",
+        }
+    ])
+    .then((answers) => {
+        const manager = new Manager(
+            answers.name,
+            answers.id,
+            answers.email,
+            answers.officeNumber
+        );
+        // All the answers pushed to teamMembers array identified above
+        teamMembers.push(manager);
+        // Calls for menu function to choose wether to add more information
+        menu();
+    })
+
+}
+
+//Starts the application
+promptManager();
 
 
 // const output = render(employees);
